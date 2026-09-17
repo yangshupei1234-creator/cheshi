c8852455a4023fc4e9f51da72facf822644229ca
yangshupei1234-creator <yangshupei1234@gmail.com>
2026-09-17 22:50:21 +0800

    test: verify file update

--- test-project/app.js
+++ test-project/app.js
@@ -2,5 +2,5 @@ const button = document.getElementById('btn');
 const result = document.getElementById('result');
 
 button.addEventListener('click', () => {
-  result.textContent = `测试成功：${new Date().toISOString()}`;
+  result.textContent = `GitHub 写入/修改测试成功：${new Date().toISOString()}`;
 });
